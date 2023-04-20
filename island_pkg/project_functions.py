d53cea9b4f3e9620a1debc8a01ec1d9916496349
Landen Brooks <brookslanden@gmail.com>
2023-04-20 13:52:28 -0400

    equal commit

--- island_pkg/project_functions.py
+++ island_pkg/project_functions.py
@@ -4,7 +4,7 @@ def battle(my_item, my_xp, fawn_xp):
         print("\nYou have used your", my_item, "to win the battle with the fawn and are able to fill your stomach.")
         show_xp(my_xp)
     else:
-        my_xp - 50
+        my_xp -= 50
         print("\nYou have lost the battle with the fawn because your", my_item, "was not powerful enough and you were not able to eat anything.")
         show_xp(my_xp)
 
